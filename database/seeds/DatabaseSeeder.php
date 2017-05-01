e8d5968586b19c4960aed8f271b8682e42e9be52
Ghulamhaider12 <haidernuman14@gmail.com>
2017-05-01 17:45:02 +0500

    my commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        /* $this->call(SiteusersTableSeeder::class);
+         $this->call(ServiceprovidersTableSeeder::class);
+         $this->call(CategoriesTableSeeder::class);*/
+          $this->call(ProfiledetailsTableSeeder::class);
     }
+  
+
 }
